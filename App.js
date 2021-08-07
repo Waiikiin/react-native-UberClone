b166d5657f1d6d54d710e83a39e2c1c59d76ded8
Wai Kin Leong <wai.kin.leong3412@gmail.com>
2021-08-07 15:47:56 -0500

    Initializing a react-native app

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import { StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const App: () => Node = () => {
+  return (
+    <>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    </>
+  );
+};
+
+export default App;
